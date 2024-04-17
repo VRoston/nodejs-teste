86a709ad0236c7ad402c4981335fdf5bc11d2812
EduardoSilvaS <eduardoss2020.ve@gmail.com>
2024-04-17 20:27:08 -0300

    tenta baixar o nodemon ai roston

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res) {
-    res.send('Boa noite');
+    res.send('Eu odeio Docker');
 });
 
 app.get('/about', function(req, res) {
